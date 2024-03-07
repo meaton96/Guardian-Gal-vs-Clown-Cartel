a104399cdba024268300c9f9a446c0dac6abda2d
meaton96 <lammara96@gmail.com>
2024-03-07 18:31:14 -0500

    Update activate.bat

--- audio_importer/env/Scripts/activate.bat
+++ audio_importer/env/Scripts/activate.bat
@@ -8,10 +8,9 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Mike\Documents\GitHub\Guardian-Gal-vs-Clown-Cartel\audio_importer\env
+set VIRTUAL_ENV=%~dp0\..
 
 if not defined PROMPT set PROMPT=$P$G
-
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
@@ -25,7 +24,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(env) 
+set VIRTUAL_ENV_PROMPT=(env)
 
 :END
 if defined _OLD_CODEPAGE (
